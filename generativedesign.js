32d64989df459a92c26a4ef7f64a843fd7bf6958
DeVuystSimon <145436026+DeVuystSimon@users.noreply.github.com>
2024-11-14 04:07:57 +0100

    Kleur achtergrond + Diamanten getekend

--- generativedesign.js
+++ generativedesign.js
@@ -7,9 +7,20 @@ canvas.height = window.innerHeight;
 function setBackground() {
     const gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
     //nog een kleur kiezen
-    gradient.addColorStop(0, '#');
-    gradient.addColorStop(1, '#');
+    gradient.addColorStop(0, '#1e3c72');
+    gradient.addColorStop(1, '#2a69ac');
     ctx.fillstyle = gradient;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-
 }
+
+function drawDiamond(ctx, cx, cy, size) {
+    ctx.beginPath();
+    ctx.moveTo(cx, cy - size * 1.2);
+    ctx.lineTo(cx + size * 0.6, cy); 
+    ctx.lineTo(cx, cy + size * 1.2); 
+    ctx.lineTo(cx - size * 0.6, cy); 
+    ctx.closePath();
+    ctx.fillStyle = '#A7D3E0'; 
+    ctx.fill();
+}
+
