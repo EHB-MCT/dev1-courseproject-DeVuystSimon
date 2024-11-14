e8cf6d7c57b8d85be22515d2c266f1643ab7408e
DeVuystSimon <145436026+DeVuystSimon@users.noreply.github.com>
2024-11-14 04:01:55 +0100

    Background kleur voorzien

--- generativedesign.js
+++ generativedesign.js
@@ -3,3 +3,13 @@ const ctx =canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+function setBackground() {
+    const gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+    //nog een kleur kiezen
+    gradient.addColorStop(0, '#');
+    gradient.addColorStop(1, '#');
+    ctx.fillstyle = gradient;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+}
