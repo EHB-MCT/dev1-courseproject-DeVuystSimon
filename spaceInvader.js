03088714a870589bf15182e6ac0c440b697a23bc
DeVuystSimon <145436026+DeVuystSimon@users.noreply.github.com>
2024-11-20 19:27:52 +0100

    Github pfp opstellen/zwart vierkant

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,21 @@
-"use strict";
+/*
+Simon De Vuyst
+*/
+/** @type {CanvasRenderingContext2D} */
+let context;
 
-alert("Draw your space invader here");
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+context = canvas.getContext('2d');
+
+alert("Start");
+
+context.fillStyle = "black";
+context.fillRect(100, 100, 300, 300);
+
+for (let i = 0; i < pinkSquarePositions.length; i++) {
+    const pos = pinkSquarePositions[i];
+    context.fillStyle = "pink";
+    context.fillRect(pos.x, pos.y, 50, 50);
+}
