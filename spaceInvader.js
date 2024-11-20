eb3a07fe574a8df6759558180341f53d8fc4c715
DeVuystSimon <145436026+DeVuystSimon@users.noreply.github.com>
2024-11-20 19:33:43 +0100

    vierkanten tekenen/pfp afwerken

--- spaceInvader.js
+++ spaceInvader.js
@@ -14,6 +14,18 @@ alert("Start");
 context.fillStyle = "black";
 context.fillRect(100, 100, 300, 300);
 
+
+const pinkSquarePositions = [
+    { x: 125, y: 175 },
+    { x: 175, y: 225 },
+    { x: 125, y: 275 },
+    { x: 175, y: 325 },
+    { x: 325, y: 175 },
+    { x: 275, y: 225 },
+    { x: 325, y: 275 },
+    { x: 275, y: 325 }
+];
+
 for (let i = 0; i < pinkSquarePositions.length; i++) {
     const pos = pinkSquarePositions[i];
     context.fillStyle = "pink";
